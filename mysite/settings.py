96152e9b5fe1dc616f1b0a3af3f31ae3a36b78ea
melihcalis <mel11h@yandex.com>
2023-10-02 22:48:22 +0300

    pff

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,11 +82,8 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# List of additional directories where Django should look for static files
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Add your app-specific static directories here
+    BASE_DIR / "static"
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
